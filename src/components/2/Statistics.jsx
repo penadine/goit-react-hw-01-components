6e2055c67947d2954df63a9ef656f5bc9325d959
penadine <penadine@gmail.com>
2023-08-13 01:05:39 +0300

    hw1

---
+++ src/components/2/Statistics.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
+
+const getRandomColor = () => {
+  const randomColor = `rgb(
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)}
+  )`;
+  return randomColor;
+};
+
+const Statistics = ({ title, stats }) => (
+  <section className={styles.statistics}>
+    {title && <h2 className={styles.title}>{title}</h2>}
+    <ul className={styles.statList}>
+      {stats.map(stat => (
+        <li
+          key={stat.id}
+          className={styles.item}
+          style={{ backgroundColor: getRandomColor() }}
+        >
+          <span className={styles.label}>{stat.label}</span>
+          <span className={styles.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+Statistics.defaultProps = {
+  title: '',
+};
+
+export default Statistics;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import React from 'react';
+import Profile from './1/Profile';
+import Statistics from './2/Statistics';
+import FriendList from './3/FriendList';
+import TransactionHistory from './4/TransactionHistory';
+import user from './1/user.json';
+import data from './2/data.json';
+import friends from './3/friends.json';
+import transactions from './4/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <div>1</div>
+      <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <br />
+       <div>2</div>
+      <div>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+      </div>
+      <br />
+      <div>3</div>
+      <div>
+        <FriendList friends={friends} />
+      </div>
+      <br />
+      <div>4</div>
+      <div>
+        <TransactionHistory items={transactions} />
+      </div>
     </div>
   );
 };
